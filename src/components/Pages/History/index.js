9306e5f3643b44ce05f68353996bbff8c2f2560a
tatasulysta <tatasulystaaa@gmail.com>
2022-04-26 21:23:52 +0700

    navbar route

---
+++ src/components/Pages/History/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function History() {
+  return (
+    <div>
+      History
+      <h2>hi</h2>
+    </div>
+  );
+}
+
+export default History;
---
+++ src/components/Pages/Home/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div style={{ height: "100vh" }}>Home</div>;
+}
+
+export default Home;
